7067cccfbc38634ed9d727e4982d771da34e28f6
Samuel Snopko <samuel@snopko.com>
2020-09-15 14:56:36 +0200

    removed old files

--- plugins/filters.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-
-Vue.filter('resize', (image, size) => {
-  if (typeof image !== 'undefined') {
-    return '//img2.storyblok.com/' + size + image.replace('//a.storyblok.com', '')
-  }
-  return null
-})
