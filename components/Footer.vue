a65801fd6373fa47b834558ce22634a9a6e7505e
Samuel Snopko <samuel@snopko.com>
2020-09-16 13:16:43 +0200

    updated layout and added header + footer

---
+++ components/Footer.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="py-10 bg-teal-100">
+    <div class="max-w-5xl mx-auto text-center">
+      <nuxt-link
+        class="text-gray-500 hover:text-gray-800 hover:underline"
+        to="/en/sitemap">
+        Sitemap
+      </nuxt-link>
+    </div>
+  </footer>
+</template>
---
+++ components/Header.vue
@@ -0,0 +1,45 @@
+<template>
+  <header class="max-w-5xl mx-auto py-8 flex">
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link
+            class="text-teal-600 hover:underline"
+            to="/">
+            Home
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link
+            class="text-teal-600 hover:underline"
+            to="/en/blog">
+            Blog
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+    <div class="flex-1">
+      <a href="/" class="block w-56 mx-auto">
+        <img src="http://a.storyblok.com/f/42016/1096x313/0353bf6654/logo2.png">
+      </a>
+    </div>
+    <nav>
+      <ul>
+        <li>
+          <nuxt-link
+            class="text-teal-600 hover:underline"
+            to="/en/blog">
+            English
+          </nuxt-link>
+        </li>
+        <li>
+          <nuxt-link
+            class="text-teal-600 hover:underline"
+            to="/de/blog">
+            German
+          </nuxt-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
